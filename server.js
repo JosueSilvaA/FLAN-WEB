3fab3efe2460d1cfaec5a3f167d48e33d7927dbe
JosueSilvaA <josuemarkcs@gmail.com>
2020-05-23 16:26:28 -0600

    ACTUALIZACION DE CONFIGURACION PARA HACER EL DEPLOY EN HEROKU x7

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 //Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
-
-
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
-
-
-
-app.get('/*', function(req,res) { 
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+ 
+const app = express();
+ 
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/ https://web-flan.herokuapp.com/'));
+ 
+app.get('/*', function(req,res) {
+ 
+res.sendFile(path.join(__dirname+'/ https://web-flan.herokuapp.com/index.html'));
 });
-
+ 
 // Start the app by listening on the default Heroku port
-
-
-console.log('CONSOLE LISTENING')
+app.listen(process.env.PORT || 8080);
