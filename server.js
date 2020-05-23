1c78130890210788f9ad4bc3f6137c253ed1ea16
JosueSilvaA <josuemarkcs@gmail.com>
2020-05-23 16:02:55 -0600

    ACTUALIZACION DE CONFIGURACION PARA HACER EL DEPLOY EN HEROKU x5

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 //Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
+
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/flan-web'));
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
 
-app.get('/', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/flan-web/src/index.html'));
+
+
+app.get('/*', function(req,res) { 
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+
+
+console.log('CONSOLE LISTENING')
