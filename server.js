8f2e4bfafa8a8f03cb37cdd3568074c9938c8402
JosueSilvaA <josuemarkcs@gmail.com>
2020-05-23 15:32:41 -0600

    ACTUALIZACION DE CONFIGURACION PARA HACER EL DEPLOY EN HEROKU x4

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/flan-web'));
+app.use(express.static(__dirname + '/flan-web'));
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/flan-web/index.html'));
+res.sendFile(path.join(__dirname+'/flan-web/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
