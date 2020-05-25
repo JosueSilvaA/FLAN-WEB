fe0ff5fd0d7975b2aedc27a424aa888e0c691add
JOSUE SILVA <josuemarkcs@gmail.com>
2020-05-24 23:22:16 -0600

    Agregado al environment.prod.ts las credenciales de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDqGvzZzBm88_drSv9e1xhKLfjMkmLFEOU",
+    authDomain: "flan-file.firebaseapp.com",
+    databaseURL: "https://flan-file.firebaseio.com",
+    projectId: "flan-file",
+    storageBucket: "flan-file.appspot.com",
+    messagingSenderId: "533244365252",
+    appId: "1:533244365252:web:1aeea4f2605ccfe866fc91",
+    measurementId: "G-7ZPDKRV8GW"
+  }
 };
